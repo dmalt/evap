fec0e27d4e8effc9304edd672498e93a34924c33
Dmitriy Altukhov <dmalt@me.com>
2014-04-24 14:45:17 +0400

    Now most of functions depend on T, because we need current T for Newton method and it doesn't equal T_w as I thought earlier. The second major improvment is that now I've finally programmed Newton method itself.

--- Evaporation.cpp
+++ Evaporation.cpp
@@ -7,26 +7,30 @@ using namespace std;
 enum{H2,O2,N2,H2O,OH,H,O,HO2,H2O2,	EC};     //
 
 
+double pow(double val, int pow){
+	double power = val;
+	for (int i = 1; i < pow; ++i)
+		power*=val;
+	return power;
+}
+
+
 Evaporation::Evaporation(double * ExtConc, double ExtTemp, double ExtPressure){
-	A[0]	=	-1649810.;
-	A[1]	=	79239.;	
-	A[2]	=	-1252.;
-	A[3]	=	6.5;
-// ****************Число компонент*******************
+/*****************Число компонент*******************/
 	CN=9;							
-// ********Критическа температура для кислорода******
+/*********Критическа температура для кислорода******/
 	T_c = 154.77; 						
-// *********Выделение памяти под массивы*************
+/*********Выделение памяти под массивы**************/
 	Y_ex	= new double[CN];			
 	Cp 		= new double[CN]; 			 
 	Y_w		= new double[CN];			
-// *****Инициализация внешних концентраций***********
+/*******Инициализация внешних концентраций**********/
 	for (int i = 0; i < CN; ++i)
 		Y_ex[i] = ExtConc[i];
 
 	T_ex 	= ExtTemp;
 	P_ex 	= ExtPressure;
-// ****************Теплоемкости компонент**************
+/****************Теплоемкости компонент*************/
 	Cp[H2]	=	16000.;	Cp[O2]	=	1670.;	Cp[N2]	=	1300;
 	Cp[H2O]	=	0.; 	Cp[OH]	=	0.;		Cp[H]	=	0.;
 	Cp[O]	=	0.; 	Cp[HO2]	=	0.; 	Cp[H2O2]=	0.;
@@ -40,7 +44,7 @@ Evaporation::~Evaporation(){
 }
 
 
-// ***Расчет теплоемкости смеси по теплоемкостям и концентрациям компонент на бесконечности***
+/***Расчет теплоемкости смеси по теплоемкостям и концентрациям компонент на бесконечности***/
 double Evaporation::GetCp_mixt_ex(){
 	double Cp_mixt_ex = 0.;
 	for (int i = 0; i < CN; ++i)
@@ -48,7 +52,7 @@ double Evaporation::GetCp_mixt_ex(){
 	return Cp_mixt_ex;
 }
 
-// ***Расчет теплоемкости смеси по теплоемкостям и концентрациям компонент на границе капли***
+/***Расчет теплоемкости смеси по теплоемкостям и концентрациям компонент на границе капли***/
 double Evaporation::GetCp_mixt_w(){
 	double GetCp_mixt_w = 0.;
 	for (int i = 0; i < CN; ++i)
@@ -56,23 +60,63 @@ double Evaporation::GetCp_mixt_w(){
 	return GetCp_mixt_w;
 }
 
-// ***Расчет тепла парообразования для заданной температуры на поверхности капли***
-double Evaporation::GetVapHeat(){
+/***Расчет тепла парообразования для заданной температуры на поверхности капли***/
+double Evaporation::GetVapHeat(double T){
 	double HL=0.;
 	if(T_w<=T_c )
-		HL=1000.*(1-0.006468*T_w)/(0.003909-0.000022*T_w);
+		HL=1000.*(1-0.006468*T)/(0.003909-0.000022*T);
 	return HL;
 }
-// ******************Расчет кси************************
-double Evaporation::GetXi(){
+/******************Расчет кси************************/
+double Evaporation::GetXi(double T){
 	double xi;
 	double Cpe 	=	GetCp_mixt_ex();
 	double Cpw 	=	GetCp_mixt_w();
-	double HL	=	GetVapHeat();
-	xi=log(1.+(Cpe*T_ex-Cpw*T_w)/(HL+Cpw*(T_w-T_av)));
+	double HL	=	GetVapHeat(T);
+	xi=log(1.+(Cpe*T_ex-Cpw*T)/(HL+Cpw*(T-T_av)));
 	return xi;
 }
 
+/*********Расчет дисбаланса уравнения для температуры*******/
+double Evaporation::GetDelta(double T){
+	return T*T;
+}
+
+double Evaporation::GetO2PartPres(double T){
+	double A[4] = {-1649810., 79239., -1252., 6.5};
+	double P_O2 = 0;
+	for (int i = 0; i < 4; ++i)
+	{
+		P_O2+=A[i] * pow(T,i);
+	}
+	return P_O2;
+}
+
+int Evaporation::SolveNewton(){
+	double T_next, T_prev, f_prime, f_next, f_prev;
+	double dT = 0.1, df;
+	T_prev = 150.;
+	T_next = T_prev - dT;     // Начальное приближение
+	int iter = 0;
+	const double a_tol = 1.e-7;
+	do{
+		/******Считаем производную********/
+		f_next = GetDelta(T_next);
+		f_prev = GetDelta(T_prev);
+		df = f_next - f_prev;
+		dT = T_next - T_prev;
+		f_prime = df/dT;
+		/********************************/
+		T_prev = T_next;
+		f_prev = f_next;
+		T_next = T_prev - f_prev/f_prime;
+		iter++;
+		cout<<".";
+	}while(fabs(T_next-T_prev) > a_tol && iter < 500);
+	cout<<endl;
+	T_w = T_next;
+	return iter;
+}
 
 int main(int argc, char  *argv[])
 {	
@@ -86,5 +130,7 @@ int main(int argc, char  *argv[])
 		}
 	double Te = 150., Pe = 1e5;
 	Evaporation dropplet(Ye, Te, Pe);
+	int iter = dropplet.SolveNewton();
+	cout<<"In "<<iter <<" iterations we`ve got T equal to "<<dropplet.T_w<<endl;
 	return 0;
 }
